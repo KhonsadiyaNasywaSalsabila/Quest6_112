ea560a1147a20819c28ad26dce79db26950db1ca
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 20:24:59 +0700

    Kerangka Dasar UI (Scaffold & TopAppBar)

---
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/view/TampilSiswa.kt
@@ -0,0 +1,63 @@
+package com.example.pertemuan8_arsitekturmvvm.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pertemuan8_arsitekturmvvm.R
+import com.example.pertemuan8_arsitekturmvvm.model.Siswa
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa(
+    statusUiSiswa: Siswa,
+    onBackButtonClicked:()->Unit
+){
+
+    Scaffold(modifier = Modifier,
+        {
+            TopAppBar(
+                title = { Text(stringResource(id = R.string.detail), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.purple_500)),
+            )
+        }
+
+    ){isiRuang->
+        Column(
+            modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Column(
+
+            ){
+
+
+            }
+
+
+        }
+
+    }
+
+}
