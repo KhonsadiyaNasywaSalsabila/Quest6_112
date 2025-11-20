bf93d24b22d3ef9243f70c41577a04b077228a78
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 20:07:35 +0700

    Menambahkan variabel untuk menyimpan data input sementara.

--- app/src/main/java/com/example/pertemuan8_arsitekturmvvm/view/FormSiswa.kt
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/view/FormSiswa.kt
@@ -42,7 +42,10 @@ fun FormSiswa(
     onSubmitButtonClicked: (MutableList<String>)-> Unit,
     modifier: Modifier = Modifier
 ) {
-
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    val listData: MutableList<String> = mutableListOf(txtNama, txtGender, txtAlamat)
 
     Scaffold(
         modifier = Modifier,
