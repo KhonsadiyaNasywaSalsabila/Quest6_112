3aff35d9f9cd89cb53daa2c1a6fdeb664759e737
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 20:46:27 +0700

    Main Activity menjalankan fungsi SiswaApp pada file Navigasi

---
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.pertemuan8_arsitekturmvvm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pertemuan8_arsitekturmvvm.ui.theme.Pertemuan8_ArsitekturMVVMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pertemuan8_ArsitekturMVVMTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    SiswaApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pertemuan8_ArsitekturMVVMTheme {
+        Greeting("Android")
+    }
+}
