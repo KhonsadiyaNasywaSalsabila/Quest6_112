3e0d6bc001ed619f2e90b70575511637d404e372
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 20:26:13 +0700

    Mapping data Siswa ke dalam List untuk ditampilkan

--- app/src/main/java/com/example/pertemuan8_arsitekturmvvm/view/TampilSiswa.kt
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/view/TampilSiswa.kt
@@ -34,6 +34,11 @@ fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBackButtonClicked:()->Unit
 ){
+    val items = listOf(
+        Pair(stringResource(R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(R.string. alamat), statusUiSiswa.alamat)
+    )
 
     Scaffold(modifier = Modifier,
         {
