c1c00cd744dcfe33c3e997492015206e260631da
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 19:14:49 +0700

    Tambah objek  statis 'DataJK' untuk data Jenis Kelamin

---
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.pertemuan8_arsitekturmvvm.model
+
+import com.example.pertemuan8_arsitekturmvvm.R
+
+object DataJK {
+    val JenisK= listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
