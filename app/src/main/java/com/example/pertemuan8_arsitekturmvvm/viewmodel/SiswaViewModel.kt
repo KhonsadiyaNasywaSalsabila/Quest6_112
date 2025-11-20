4d0d2773e9f1f8831f43d96f4c430cc0e2ceb2fc
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-20 19:32:44 +0700

    menyimpan state data siswa secara real-time.

---
+++ app/src/main/java/com/example/pertemuan8_arsitekturmvvm/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan8_arsitekturmvvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan8_arsitekturmvvm.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+
+
+}
